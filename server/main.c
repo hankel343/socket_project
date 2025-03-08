aa4163d2b5cc376af1a411cdf8a1855ea2e04af5
Hankel Haldin <hankel343@gmail.com>
2025-03-08 17:13:52 -0600

    add header files

--- server/main.c
+++ server/main.c
@@ -1,6 +1,13 @@
-#include <sys/socket.h>
+#include <sys/socket.h>		// socket functions
+#include <netinet/in.h>		// address structs
+#include <arpa/inet.h>		// common network conversion functions (i.e, inet_ntoa)
+#include <unistd.h>		// POSIX API functions (e.g, close(), fork(), etc)
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
 	
+	// create a socket
+
 	return 0;
 }
