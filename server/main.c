0fc96589a925f935d5d53e9c7b4dfe125f650a0c
Hankel Haldin <hankel343@gmail.com>
2025-03-08 21:30:08 -0600

    create server socket

--- server/main.c
+++ server/main.c
@@ -7,7 +7,22 @@
 
 int main(int argc, char** argv) {
 	
+	int sockfd;
+
 	// create a socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP
+	
+	// set up address struct
+	struct addrinfo addr;
+
+	memset(&addr, 0, sizeof(struct addrinfo));
+
+// 	status = getsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &optval, &optlen);
+// 
+// 	if ( status != 0 ) {
+// 		perror("getsockopt");
+// 		return 1;
+// 	}
 
 	return 0;
 }
